6e4eae278c22f11a1c166d615cf5cdefc2b81964
Frederik Lütkes <frederik.luetkes@googlemail.com>
2020-08-03 18:11:26 +0200

    improve song details fragment

--- app/src/main/java/org/gateshipone/malp/application/fragments/serverfragments/SongDetailsDialog.java
+++ app/src/main/java/org/gateshipone/malp/application/fragments/serverfragments/SongDetailsDialog.java
@@ -27,31 +27,41 @@ import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.TypedValue;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
 import org.gateshipone.malp.R;
+import org.gateshipone.malp.application.adapters.SongDetailsAdapter;
 import org.gateshipone.malp.application.fragments.ErrorDialog;
 import org.gateshipone.malp.application.utils.FormatHelper;
-import org.gateshipone.malp.application.utils.ThemeUtils;
 import org.gateshipone.malp.mpdservice.handlers.serverhandler.MPDQueryHandler;
 import org.gateshipone.malp.mpdservice.mpdprotocol.mpdobjects.MPDTrack;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SongDetailsDialog extends DialogFragment {
     public static final String EXTRA_FILE = "file";
     public static final String EXTRA_HIDE_ADD = "hide_add";
 
     private MPDTrack mFile;
+
     private boolean mHideAdd;
+
+    private SongDetailsAdapter mAdapter;
+
+    public static SongDetailsDialog createDialog(@NonNull MPDTrack track, boolean hideAdd) {
+        SongDetailsDialog dialog = new SongDetailsDialog();
+        Bundle args = new Bundle();
+        args.putParcelable(SongDetailsDialog.EXTRA_FILE, track);
+        args.putBoolean(EXTRA_HIDE_ADD, hideAdd);
+
+        dialog.setArguments(args);
+        return dialog;
+    }
 
     @NonNull
     @Override
@@ -63,99 +73,54 @@ public class SongDetailsDialog extends DialogFragment {
             mHideAdd = args.getBoolean(EXTRA_HIDE_ADD);
         }
 
+        final List<SongDetailsAdapter.SongDetailsItem> items = createItems();
+
+        mAdapter = new SongDetailsAdapter(requireContext(), items);
+
         final AlertDialog.Builder builder = new AlertDialog.Builder(requireActivity());
 
-        final LayoutInflater inflater = getLayoutInflater();
-        final View rootView = inflater.inflate(R.layout.fragment_song_details, null);
+        builder.setAdapter(mAdapter, (dialog, which) -> {
+            final SongDetailsAdapter.SongDetailsItem item = mAdapter.getItem(which);
 
-        TextView mTrackNo = rootView.findViewById(R.id.song_detail_text_track_no);
-        TextView mTrackDisc = rootView.findViewById(R.id.song_detail_text_disc_no);
-        TextView mTrackDuration = rootView.findViewById(R.id.song_detail_text_song_duration);
+            final MPDTrack.StringTagTypes tag = item.getTagType();
 
-        TextView mTrackURI = rootView.findViewById(R.id.song_detail_text_track_uri);
+            // add click listener only for MusicBrainz Links
+            if (tag != null && tag.name().contains("MBID")) {
+                String url = null;
+                switch (tag) {
+                    case TRACK_MBID:
+                        url = "https://www.musicbrainz.org/recording/";
+                        break;
+                    case ALBUM_MBID:
+                        url = "https://www.musicbrainz.org/release/";
+                        break;
+                    case WORK_MBID:
+                        url = "https://www.musicbrainz.org/work/";
+                        break;
+                    case ARTIST_MBID:
+                    case ALBUMARTIST_MBID:
+                        url = "https://www.musicbrainz.org/artist/";
+                        break;
+                    default:
+                        break;
+                }
 
+                if (url != null) {
+                    final String mbidURL = url + item.getDisplayValue();
 
-        if (null != mFile) {
-            if (mFile.getAlbumTrackCount() != 0) {
-                mTrackNo.setText(getResources().getString(R.string.track_number_template, mFile.getTrackNumber(), mFile.getAlbumTrackCount()));
-            } else {
-                mTrackNo.setText(String.valueOf(mFile.getTrackNumber()));
-            }
+                    Intent urlIntent = new Intent(Intent.ACTION_VIEW);
+                    urlIntent.setData(Uri.parse(mbidURL));
 
-            if (mFile.getAlbumDiscCount() != 0) {
-                mTrackDisc.setText(getResources().getString(R.string.track_number_template, mFile.getDiscNumber(), mFile.getAlbumDiscCount()));
-            } else {
-                mTrackDisc.setText(String.valueOf(mFile.getDiscNumber()));
-            }
-            mTrackDuration.setText(FormatHelper.formatTracktimeFromS(mFile.getLength()));
-
-            mTrackURI.setText(mFile.getPath());
-
-            LinearLayout textViewLL = rootView.findViewById(R.id.tag_linear_layout);
-
-            final float scale = getResources().getDisplayMetrics().density;
-            final int topPadding = (int) (getResources().getDimension(R.dimen.material_content_spacing) *
-                    scale + 0.5f);
-            for (MPDTrack.StringTagTypes tag : MPDTrack.StringTagTypes.values()) {
-                // Check all tags for values
-                String tagValue = mFile.getStringTag(tag);
-                if (!tagValue.isEmpty()) {
-                    TextView tagHeader = new TextView(getContext());
-                    tagHeader.setText(getResources().getString(R.string.tag_header_template, tag.name()));
-                    tagHeader.setPadding(0, topPadding, 0, 0);
-                    tagHeader.setTextColor(ThemeUtils.getThemeColor(requireContext(), R.attr.malp_color_text_background_secondary));
-                    tagHeader.setTextSize(TypedValue.COMPLEX_UNIT_PX, getResources().getDimension(R.dimen.material_font_style_size_body_1));
-                    textViewLL.addView(tagHeader);
-
-                    TextView tagValueView = new TextView(getContext());
-                    tagValueView.setText(tagValue);
-                    tagValueView.setTextSize(TypedValue.COMPLEX_UNIT_PX, getResources().getDimension(R.dimen.material_font_style_size_body_2));
-
-                    // MusicBrainz linking
-                    if (tag.name().contains("MBID")) {
-                        String url = null;
-                        switch (tag) {
-                            case TRACK_MBID:
-                                url = "https://www.musicbrainz.org/recording/" + mFile.getStringTag(MPDTrack.StringTagTypes.TRACK_MBID);
-                                break;
-                            case ALBUM_MBID:
-                                url = "https://www.musicbrainz.org/release/" + mFile.getStringTag(MPDTrack.StringTagTypes.ALBUM_MBID);
-                                break;
-                            case WORK_MBID:
-                                url = "https://www.musicbrainz.org/work/" + mFile.getStringTag(MPDTrack.StringTagTypes.WORK_MBID);
-                                break;
-                            case ARTIST_MBID:
-                                url = "https://www.musicbrainz.org/artist/" + mFile.getStringTag(MPDTrack.StringTagTypes.ARTIST_MBID);
-                                break;
-                            case ALBUMARTIST_MBID:
-                                url = "https://www.musicbrainz.org/artist/" + mFile.getStringTag(MPDTrack.StringTagTypes.ALBUMARTIST_MBID);
-                                break;
-                            default:
-                                break;
-                        }
-
-                        if (url != null) {
-                            final String mbidURL = url;
-                            tagValueView.setOnClickListener(v -> {
-                                Intent urlIntent = new Intent(Intent.ACTION_VIEW);
-                                urlIntent.setData(Uri.parse(mbidURL));
-
-                                try {
-                                    startActivity(urlIntent);
-                                } catch (ActivityNotFoundException e) {
-                                    final ErrorDialog noBrowserFoundDlg = ErrorDialog.newInstance(R.string.dialog_no_browser_found_title, R.string.dialog_no_browser_found_message);
-                                    noBrowserFoundDlg.show(((AppCompatActivity) requireActivity()).getSupportFragmentManager(), "BrowserNotFoundDlg");
-                                }
-                            });
-                        }
+                    try {
+                        startActivity(urlIntent);
+                    } catch (ActivityNotFoundException e) {
+                        final ErrorDialog noBrowserFoundDlg = ErrorDialog.newInstance(R.string.dialog_no_browser_found_title, R.string.dialog_no_browser_found_message);
+                        noBrowserFoundDlg.show(requireActivity().getSupportFragmentManager(), "BrowserNotFoundDlg");
                     }
-
-                    textViewLL.addView(tagValueView);
                 }
             }
-        }
+        });
 
-        builder.setView(rootView);
         if (!mHideAdd) {
             builder.setPositiveButton(R.string.action_add, (dialog, which) -> {
                 if (null != mFile) {
@@ -171,13 +136,56 @@ public class SongDetailsDialog extends DialogFragment {
         return builder.create();
     }
 
-    public static SongDetailsDialog createDialog(@NonNull MPDTrack track, boolean hideAdd) {
-        SongDetailsDialog dialog = new SongDetailsDialog();
-        Bundle args = new Bundle();
-        args.putParcelable(SongDetailsDialog.EXTRA_FILE, track);
-        args.putBoolean(EXTRA_HIDE_ADD, hideAdd);
+    private List<SongDetailsAdapter.SongDetailsItem> createItems() {
+        final List<SongDetailsAdapter.SongDetailsItem> items = new ArrayList<>();
 
-        dialog.setArguments(args);
-        return dialog;
+        // create custom items
+        String customKey;
+        String customValue;
+
+        // track number
+        customKey = getString(R.string.song_track_no);
+
+        if (mFile.getAlbumTrackCount() != 0) {
+            customValue = getString(R.string.track_number_template, mFile.getTrackNumber(), mFile.getAlbumTrackCount());
+        } else {
+            customValue = String.valueOf(mFile.getTrackNumber());
+        }
+
+        items.add(new SongDetailsAdapter.SongDetailsItem(customKey, customValue));
+
+        // tracks disc
+        customKey = getString(R.string.song_disc);
+
+        if (mFile.getAlbumTrackCount() != 0) {
+            customValue = getString(R.string.track_number_template, mFile.getDiscNumber(), mFile.getAlbumDiscCount());
+        } else {
+            customValue = String.valueOf(mFile.getDiscNumber());
+        }
+
+        items.add(new SongDetailsAdapter.SongDetailsItem(customKey, customValue));
+
+        // track duration
+        customKey = getString(R.string.song_duration);
+        customValue = FormatHelper.formatTracktimeFromS(mFile.getLength());
+
+        items.add(new SongDetailsAdapter.SongDetailsItem(customKey, customValue));
+
+        // track uri
+        customKey = getString(R.string.song_uri);
+        customValue = mFile.getPath();
+
+        items.add(new SongDetailsAdapter.SongDetailsItem(customKey, customValue));
+
+        // create default items
+        for (MPDTrack.StringTagTypes tag : MPDTrack.StringTagTypes.values()) {
+            final String tagValue = mFile.getStringTag(tag);
+
+            if (!tagValue.isEmpty()) {
+                items.add(new SongDetailsAdapter.SongDetailsItem(tag, getString(R.string.tag_header_template, tag.name()), tagValue));
+            }
+        }
+
+        return items;
     }
 }
